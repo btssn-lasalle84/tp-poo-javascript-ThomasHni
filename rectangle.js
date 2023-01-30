41f53f08119bbc6211083fe7b3f3df9e728bf58c
tvaira <tvaira@free.fr>
2023-01-30 17:45:26 +0100

    Initialise fichier rectngle.js

--- rectangle.js
+++ rectangle.js
@@ -1,30 +1,5 @@
 #!/usr/bin/env node
 
-function Rectangle(largeur, longueur) 
-{
-    this.largeur = largeur;
-    this.longueur = longueur;
-    this.affiche = function() 
-    {
-        if (this.largeur !== undefined && this.longueur !== undefined)
-            console.log(this.largeur + " x " +  this.longueur);
-    }
-    this.aire = function() 
-    {
-        return this.largeur * this.longueur;
-    }
-    this.perimetre = function()
-    {
-        return (this.largeur * 2) + (this.longueur * 2);
-    }
-}
-
-var rectangle = new Rectangle();
-rectangle.affiche();
-
-var carre = new Rectangle(100, 100);
-carre.affiche();
-console.log("aire = " + carre.aire());
-console.log("perimetre = " + carre.perimetre());
+// TODO
 
 module.exports = Rectangle;
